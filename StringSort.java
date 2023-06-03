a1abfae1fbf2010235d7fdf44f31d30456fdc5ad
easternsavageman <easternsavageman@gmail.com>
2023-06-03 08:13:26 -0600

    Create StringSort.java

---
+++ StringSort.java
@@ -0,0 +1,18 @@
+/* Complete the following programming assignment and submit your code (.java)  by uploading the file(s).
+
+Write a program that sorts Strings. 
+The program should prompt the user for the number of Strings to read, 
+and then read the Strings from standard input one at a time. 
+Then it should sort the Strings into alphabetical order using any sorting algorithm with which you are familiar, 
+and print them to the console. 
+The commentary material on console I/O, arrays, and the String.compareTo() method should all be useful, 
+so be sure to read the commentary! 
+Do not use any classes in the java.util package; 
+specifically, do not use java.util.Arrays or java.util.Vector. */
+
+
+public class StringSort {
+    // How many strings do you want to read
+    // read strings one at a time from input
+    // sort the strings alphabetically
+}
