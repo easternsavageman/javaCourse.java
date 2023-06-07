eff4730bb5e1898b16efd7327507ef17c9eca356
easternsavageman <easternsavageman@gmail.com>
2023-06-06 20:09:10 -0600

    Update StringSort.java

--- StringSort.java
+++ StringSort.java
@@ -1,18 +1,46 @@
-/* Complete the following programming assignment and submit your code (.java)  by uploading the file(s).
+// Module 2 Programming Assignment
 
-Write a program that sorts Strings. 
-The program should prompt the user for the number of Strings to read, 
-and then read the Strings from standard input one at a time. 
-Then it should sort the Strings into alphabetical order using any sorting algorithm with which you are familiar, 
-and print them to the console. 
-The commentary material on console I/O, arrays, and the String.compareTo() method should all be useful, 
-so be sure to read the commentary! 
-Do not use any classes in the java.util package; 
-specifically, do not use java.util.Arrays or java.util.Vector. */
-
+// put this at the very top of your file: 
+import java.io.*;
 
 public class StringSort {
-    // How many strings do you want to read
-    // read strings one at a time from input
-    // sort the strings alphabetically
+    // change the first line of main to look like this 
+    public static void main(String [] argv) throws IOException
+    {
+        // then, in your main() function:
+        BufferedReader stdin =
+        new BufferedReader(new InputStreamReader(System.in), 1);
+        // then, to get a line of user input
+        System.out.println("How many strings do you want to read?");
+        // ... or to read an integer 
+        int numString = Integer.parseInt(stdin.readLine());
+        // getting the string
+        String [] myArray = new String[numString];
+        System.out.println(myArray);
+        // read strings one at a time from input
+        for (int i=0; i<numString; i++) {
+            if (i==0){
+                System.out.println("Please enter string: ");
+                //as a string: 
+                String theString = stdin.readLine();
+                myArray[i] = theString;
+            } else {
+                // sort the strings alphabetically
+                System.out.println("Please enter string: ");
+                //as a string: 
+                String theString = stdin.readLine();
+                for (int j=numString-1; j==0; j--) {
+                    if (theString.compareTo(myArray[j-1])<0) {
+                        myArray[j] = myArray[j-1];
+                        myArray[j-1] = theString;
+                    } else {
+                        myArray[j] = theString;
+                    }
+                }
+            }
+        }
+        for (int k=numString; k<numString; k++) {
+            System.out.println(myArray[k]); 
+        }  
+    }
 }
