88509b2be11a238088a210240b61f3902c5896f4
easternsavageman <easternsavageman@gmail.com>
2023-06-08 07:33:05 -0600

    Create cashRegister.java

---
+++ cashRegister.java
@@ -0,0 +1,18 @@
+// Write a cash register class. It should have the following methods:
+// AddTransaction(amount) – add an amount to a running total and increments the number of transactions
+// TransactionCount( ) – returns the number of transactions received
+// Total( ) – returns the sum of the amounts of the transactions
+// ResetTransactions( ) – sets the total amount and transaction count to 0
+// Register Count( ) – returns the number of cash registers created
+// Next, write an application that instantiates at least two cash register objects to prove that your class works.
+// P.S.: Don't worry about storing each individual transaction, and we are dealing only with totals. No need to handle individual orders/items per transaction
+
+
+public class cashRegister {
+    
+public static void main(String[] argv)   
+{ 
+    // String stringReversed = reverse("perpendicular"); 
+    // System.out.println(stringReversed);
+}  
+}
