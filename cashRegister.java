8ef7370998e783d3d5f94c51b9337beb0ec625df
easternsavageman <easternsavageman@gmail.com>
2023-06-12 12:02:22 -0600

    Update cashRegister.java

--- cashRegister.java
+++ cashRegister.java
@@ -1,18 +1,57 @@
-// Write a cash register class. It should have the following methods:
-// AddTransaction(amount) – add an amount to a running total and increments the number of transactions
-// TransactionCount( ) – returns the number of transactions received
-// Total( ) – returns the sum of the amounts of the transactions
-// ResetTransactions( ) – sets the total amount and transaction count to 0
-// Register Count( ) – returns the number of cash registers created
+import java.io.*;
+
+// Write a cash register class.
+public class cashRegister
+{
+    private static int numCashRegistersCreated = 0;
+    private static int numTransactionsReceived = 0;
+    private static double sumTransactionsAmount = 0.0;
+    
+    public cashRegister() {
+        ++ numCashRegistersCreated;
+    }
+
+    // add an amount to a running total and increments the number of transactions
+    public void AddTransaction(double amount) {
+        sumTransactionsAmount += amount;
+        ++ numTransactionsReceived;
+    }
+
+    // sets the total amount and transaction count to 0
+    public void ResetTransactions() {
+        sumTransactionsAmount = 0.0;
+        numTransactionsReceived = 0;
+    }
+    
+    // returns the number of cash registers created
+    public static int RegisterCount() {
+        return numCashRegistersCreated;
+    }
+
+    // returns the number of transactions received
+    public static int TransactionCount() {
+        return numTransactionsReceived;
+    }
+
+    // returns the sum of the amounts of the transactions
+    public static double Total() {
+        return sumTransactionsAmount;
+    }
+
 // Next, write an application that instantiates at least two cash register objects to prove that your class works.
 // P.S.: Don't worry about storing each individual transaction, and we are dealing only with totals. No need to handle individual orders/items per transaction
+    public static void main(String[] argv) 
+        throws IOException
+    {
+        cashRegister McDonalds = new cashRegister();
+        McDonalds.AddTransaction(11.0);
+        McDonalds.AddTransaction(12.0);
 
+        cashRegister BurgerKing = new cashRegister();
+        BurgerKing.AddTransaction(13.0);
 
-public class cashRegister {
-    
-public static void main(String[] argv)   
-{ 
-    // String stringReversed = reverse("perpendicular"); 
-    // System.out.println(stringReversed);
-}  
+        System.out.println("The number of registers is " + RegisterCount());
+        System.out.println("The total number of transactions is " + TransactionCount());
+        System.out.println("The total amount for all of the transactions is $" + Total());
+    }
 }
