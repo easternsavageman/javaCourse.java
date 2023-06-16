2bb86c5142814eaf44d56039f43efb70c5d0d31b
easternsavageman <easternsavageman@gmail.com>
2023-06-16 08:08:17 -0600

    Update Module5.java

--- Module5.java
+++ Module5.java
@@ -1,5 +1,29 @@
 // Part 1
 
+/*
+SaturnSL1 sl = new SaturnSL1( ); // will compile WITHOUT errors, will run WITHOUT errors			
+HondaCivic hc = new HondaCivic( ); // will compile WITHOUT errors, will run WITHOUT errors
+Japanese jp = new Japanese( ); // will compile WITH errors, reason for error: Japanese is not a class object, therefore it cannot be instantiated		
+German gr = new MercedesC230( ); // will compile WITHOUT errors, will run WITHOUT errors	
+ChevyS10 cs = new Truck( );	// will compile WITH errors, reason for error: Truck is not a class object, therefore it cannot be instantiated	
+SubaruOutback sb = new SubaruOutback( ); // will compile WITHOUT errors, will run WITHOUT errors		
+Domestic dm = sl; // will compile WITHOUT errors, will run WITHOUT errors
+dm = hc; // will compile WITH errors, reason for error: Domestic is not implemented by the class HondaCivic		
+dm = (Domestic) hc;	// will compile WITHOUT errors, will run WITHOUT errors	
+dm = cs; // will compile WITHOUT errors, will run WITHOUT errors			
+dm = (Domestic) cs; // will compile WITHOUT errors, will run WITHOUT errors 
+Import im = sb;	// will compile WITHOUT errors, will run WITHOUT errors		
+gr = im; // will compile WITH errors, will run WITHOUT errors			
+gr = (German) im; // will compile WITH errors, reason for error: class SubaruOutback is not implemented by interface German
+jp = im; // will compile WITHOUT errors, will run WITHOUT errors			
+jp = (German) im; // will compile WITH errors, reason for error: class SubaruOutback is not implemented by interface German			
+jp = (Japanese) im;	// will compile WITHOUT errors, will run WITHOUT errors
+Automobile a = cs; // will compile WITHOUT errors, will run WITHOUT errors
+a = hc; // will compile WITHOUT errors, will run WITHOUT errors	
+ */
+
+// Part 2
+
 interface Domestic { }
 interface Import { }
 interface Japanese extends Import { }
@@ -7,7 +31,9 @@ interface German extends Import { }
 interface Detroit extends Domestic { }
 interface SpringHill extends Domestic { }
    
-interface Vehicle { }
+interface Vehicle {
+    int getWeightInPounds( );
+}
 interface Automobile extends Vehicle { }
 interface LargeAutomobile extends Vehicle { }
 interface Sedan extends Automobile { }
@@ -16,56 +42,98 @@ interface Truck extends LargeAutomobile { }
 interface Compact extends Automobile { }
 interface SportsUtilityVehicle extends Truck, Van { }
    
-class SaturnSL1 implements SpringHill, Sedan { }
-class HondaCivic implements Japanese, Compact { }
-class MercedesC230 implements German, Sedan { }
-class ChevyS10 implements Detroit, Truck { }
-class SubaruOutback implements Japanese, SportsUtilityVehicle { }
+class SubaruOutback implements Japanese, SportsUtilityVehicle {
+    public int getWeightInPounds() {
+        return 1000;
+    } }
+class SaturnSL1 implements SpringHill, Sedan {
+    public int getWeightInPounds() {
+        return 1000;
+    } }
+class HondaCivic implements Japanese, Compact {
+    public int getWeightInPounds() {
+        return 1000;
+    } }
+class MercedesC230 implements German, Sedan {
+    public int getWeightInPounds() {
+        return 1000;
+    } }
+class ChevyS10 implements Detroit, Truck {
+    public int getWeightInPounds() {
+        return 2500;
+    } }
 
-/* For each of the following lines of code, tell me whether the assignment will compile, and if so, if it will succeed or fail at runtime. 
-For any that fail to compile, pretend that the right-hand side of that statement is null, and answer the remaining questions assuming this change has been made. 
-For example, in the following,
+class ParkingGarage {
 
- 1. Cat c = new Dog( );
- 2. c = new PersianCat( )
+    private static int numParkingGaragesCreated = 0;
+    private static int numCars = 0;
+    private static int totalWeight = 0;
 
-assignment number 1 will fail to compile. Answer number 2 assuming that 1 reads
-Cat c = null; 
-*/
+    public ParkingGarage() {
+        ++ numParkingGaragesCreated;
+    }
 
-SaturnSL1 sl = new SaturnSL1( );			
-HondaCivic hc = new HondaCivic( );			
-Japanese jp = new Japanese( );			
-German gr = new MercedesC230( );			
-ChevyS10 cs = new Truck( );			
-SubaruOutback sb = new SubaruOutback( );			
-Domestic dm = sl;			
-dm = hc;			
-dm = (Domestic) hc;			
-dm = cs;			
-dm = (Domestic) cs;			
-Import im = sb;			
-gr = im;			
-gr = (German) im;			
-jp = im;			
-jp = (German) im;			
-jp = (Japanese) im;			
-Automobile a = cs;			
-a = hc;
+    // add an amount to a running total and increments the amount of weight in the garage
+    public void AddWeight(int weight) {
+        if ((totalWeight + weight) > 25000) {
+            System.out.println("Could not accept car");
+        } else {
+            if (numCars >= 20) {
+            System.out.println("Could not accept car");
+            } else {
+                totalWeight += weight;
+                ++ numCars;
+            } 
+        }
+    }
+    
+    // returns the number of parking garages created
+    public static int GarageCount() {
+        return numParkingGaragesCreated;
+    }
+    
+    // returns the number of cars in the garage
+    public static int CarCount() {
+        return numCars;
+    }
 
-// Part 2
+    // returns the sum of all the cars weights
+    public static int TotalWeight() {
+        return totalWeight;
+    }
 
-/* Add the following declaration to interface Vehicle:
+    public static void main(String[] argv)
+    {
+        ParkingGarage Spot = new ParkingGarage();
 
-int getWeightInPounds( );
+        Vehicle red = new HondaCivic();
+        Vehicle blue = new ChevyS10();
+        Vehicle green = new ChevyS10();
+        Vehicle brown = new ChevyS10();
+        Vehicle black = new ChevyS10();
+        Vehicle purple = new ChevyS10();
+        Vehicle yellow = new ChevyS10();
+        Vehicle orange = new ChevyS10();
+        Vehicle grey = new ChevyS10();
+        Vehicle maroon = new ChevyS10();
+        Vehicle lime = new MercedesC230();
+        Vehicle pink = new SaturnSL1();
+        
+        Spot.AddWeight(red.getWeightInPounds());
+        Spot.AddWeight(blue.getWeightInPounds());
+        Spot.AddWeight(green.getWeightInPounds());
+        Spot.AddWeight(brown.getWeightInPounds());
+        Spot.AddWeight(black.getWeightInPounds());
+        Spot.AddWeight(purple.getWeightInPounds());
+        Spot.AddWeight(yellow.getWeightInPounds());
+        Spot.AddWeight(orange.getWeightInPounds());
+        Spot.AddWeight(grey.getWeightInPounds());
+        Spot.AddWeight(maroon.getWeightInPounds());
+        Spot.AddWeight(lime.getWeightInPounds());
+        Spot.AddWeight(pink.getWeightInPounds());
 
-Then add the minumum amount of additional code to make all the classes that implement Vehicle compile again. 
-Assume that a small car weighs 1,000 pounds, and a large pickup truck weighs 2,500 pounds. 
-Try to write as little redundant code as possible. You may introduce new classes if this would help meet this goal.
-
-Write a class ParkingGarage which simulates a garage for parking Vehicles. 
-The garage should have have enough capacity to park 20 Vehicles or 25,000 pounds. 
-It should refuse to park any Vehicle that would push the garage over either of these limits. 
-Note that ParkingGarage should be a well-behaved class; in particular, it should be possible to create multiple independent ParkingGarages in one program.
-
-ParkingGarage should have a main routine which demonstrates its capabilities. main( ) need not accept any user input. */
+        System.out.println("The number of parking garages is " + GarageCount());
+        System.out.println("The total number of cars in the garage is " + CarCount());
+        System.out.println("The total amount weight for all of the cars in the garage is " + TotalWeight());
+    }
+}
