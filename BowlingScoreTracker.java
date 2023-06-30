196fcc591c4485a76ffb882b189b92943bb41a24
easternsavageman <easternsavageman@gmail.com>
2023-06-30 10:39:57 -0600

    Update BowlingScoreTracker.java

--- BowlingScoreTracker.java
+++ BowlingScoreTracker.java
@@ -1,12 +1,36 @@
-/*
-Use the collections framework and generic types to create a bowling score tracker collection. 
-Design a data structure that will be used as elements in a collection. 
-Each element should contain a unique name of the bowler and a list of scores and dates of the games they have played.
-Then print to the screen each:
-    Bowler name
-    Number of games
-    Average score of all of the games
-    Score of their last game
-    Date of their last game
-Provide test cases to prove your collection works.
- */
+import java.util.*;
+
+public class BowlingScoreTracker {
+
+    private String playerName = "";
+    private int playerNumGames = 0;
+    private double playerAvgScore = 0.0;
+    private int playerLastGameScore = 0;
+    private String playerDateLastGame = "";
+
+    public BowlingScoreTracker(String bowlerName, int numGames, double avgScore, int lastGameScore, String dateLastGame) {
+        playerName = bowlerName;
+        playerNumGames = numGames;
+        playerAvgScore = avgScore;
+        playerLastGameScore = lastGameScore;
+        playerDateLastGame = dateLastGame;
+    }
+   
+  public static void main(String[] argv)  {
+    ArrayList<BowlingScoreTracker> objlist = new ArrayList<BowlingScoreTracker>();
+    
+    BowlingScoreTracker tom = new BowlingScoreTracker("Tom",5, 150.5, 160, "02/15/2023");
+    BowlingScoreTracker bob = new BowlingScoreTracker("Bob",10, 175.5, 200, "01/20/2023");
+    objlist.add(tom);
+    objlist.add(bob);
+
+    for (BowlingScoreTracker player: objlist) {
+        System.out.println("Bowler Name: " + player.playerName);
+        System.out.println("Bowler Number of games: " + player.playerNumGames);
+        System.out.println("Bowler Average Score of all of their games: " + player.playerAvgScore);
+        System.out.println("Bowler Score of their last game: " + player.playerLastGameScore);
+        System.out.println("Bowler Date of their last game: " + player.playerDateLastGame);
+        System.out.println("\n");
+    }
+}
+}
